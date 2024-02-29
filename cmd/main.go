80d32b76b6b82aeb7f95a6371c1aa67f5ab79a33
Mateus <mateuscavalcant7@gmail.com>
2024-02-29 08:33:11 -0300

    config main

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,33 @@
 package main
 
+import
+(
+	"github.com/joho/godotenv"
+	"log"
+	"social-network-server/pkg/database"
+	"github.com/gin-gonic/gin"
+	"social-network-server/api/routes"
+
+)
+
+
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	database.InitializeDB()
+
+	r := gin.Default()
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	routes.InitRoutes(r.Group("/"))
+	err = http.ListenAndServe(":8080", r)
+	if err != nil {
+		log.Fatal("Failed to start server: ", err)
+	}
+
 
 }
